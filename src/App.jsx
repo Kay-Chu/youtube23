6c7ecbc91e902f1102ec8700928b2727835c16af
Kaying <39971215+Kay-Chu@users.noreply.github.com>
2023-09-18 00:36:19 +0800

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  color: white;
+  background: url("./img/bg.jpeg");
+  scrollbar-width: none;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
   )
 }
 
